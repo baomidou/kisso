a79255326812b5bad90b552e30c6a051f43b6b4e
青苗 <243194995@qq.com>
2015-06-15 09:50:32 +0800

    新增 properties 提取方法

--- kisso/src/main/java/wang/leq/sso/common/util/PropertiesUtil.java
+++ kisso/src/main/java/wang/leq/sso/common/util/PropertiesUtil.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (c) 2011-2014, hubin (243194995@qq.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wang.leq.sso.common.util;
 
@@ -31,20 +31,25 @@ import org.slf4j.LoggerFactory;
 public class PropertiesUtil {
 
 	private final static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
-	private Properties properties;
 
-	public PropertiesUtil(Properties properties) {
+	private final Properties properties;
+
+
+	public PropertiesUtil( Properties properties ) {
 		this.properties = properties;
 	}
-	
-	public PropertiesUtil(Properties mergeProperties, String productionMode) {
+
+
+	public PropertiesUtil( Properties mergeProperties, String productionMode ) {
 		this.properties = extractProductionMode(mergeProperties, productionMode);
 	}
 
-	public String get(String key) {
+
+	public String get( String key ) {
 		return properties.getProperty(key);
 	}
-	
+
+
 	/**
 	 * properties 	提取当前模式配置
 	 * --------------------
@@ -53,88 +58,109 @@ public class PropertiesUtil {
 	 * online_mode	生产模式
 	 * --------------------
 	 */
-	public static Properties extractProductionMode(Properties mergeProperties,
-			String productionMode) {
-		if (mergeProperties == null) {
+	public static Properties extractProductionMode( Properties prop, String productionMode ) {
+		return extractProductionMode(prop, productionMode, "online_mode");
+	}
+
+
+	/**
+	 * properties 提取当前模式配置
+	 * <p>
+	 * @param prop
+	 * 配置文件 Properties
+	 * @param productionMode
+	 * 				当前配置模式
+	 * @param defaultMode
+	 * 				默认模式
+	 * @return 
+	 */
+	public static Properties extractProductionMode( Properties prop, String productionMode, String defaultMode ) {
+		if ( prop == null || productionMode == null || defaultMode == null ) {
 			return null;
 		}
-		
+
 		/**
 		 * 获取路由规则, 系统属性设置mode优先
 		 */
 		Properties properties = new Properties();
 		String mode = System.getProperty(productionMode);
-		if (mode == null) {
-			String str = mergeProperties.getProperty(productionMode);
-			mode = (str != null) ? str : "online_mode";
+		if ( mode == null ) {
+			String str = prop.getProperty(productionMode);
+			mode = (str != null) ? str : defaultMode;
 		}
 		logger.info("production.mode={}", mode);
 		properties.put(productionMode, mode);
-		Set<Entry<Object, Object>> es = mergeProperties.entrySet();
-		for (Entry<Object, Object> entry : es) {
+		Set<Entry<Object, Object>> es = prop.entrySet();
+		for ( Entry<Object, Object> entry : es ) {
 			String key = (String) entry.getKey();
 			int idx = key.lastIndexOf("_mode");
 			String realKey = key;
-			if (idx > 0) {
-				if (key.contains(mode)) {
+			if ( idx > 0 ) {
+				if ( key.contains(mode) ) {
 					realKey = key.substring(0, key.lastIndexOf("_" + mode));
 				} else {
 					realKey = null;
 				}
 			}
-			if (realKey != null && !properties.containsKey(realKey)) {
+			if ( realKey != null && !properties.containsKey(realKey) ) {
 				Object value = null;
-				if (idx > 0) {
-					value = mergeProperties.get(realKey + "_" + mode);
+				if ( idx > 0 ) {
+					value = prop.get(realKey + "_" + mode);
 				} else {
-					value = mergeProperties.get(realKey);
+					value = prop.get(realKey);
 				}
-				if (value != null) {
+				if ( value != null ) {
 					properties.put(realKey, value);
 				} else {
-					throw new RuntimeException("impossible empty property for "
-							+ realKey);
+					throw new RuntimeException("impossible empty property for " + realKey);
 				}
 			}
 		}
 		return properties;
 	}
 
-	public String get(String key, String defaultVal) {
+
+	public String get( String key, String defaultVal ) {
 		String val = get(key);
 		return val == null ? defaultVal : val;
 	}
 
-	public String findValue(String... keys) {
-		for (String key : keys) {
+
+	public String findValue( String... keys ) {
+		for ( String key : keys ) {
 			String value = get(key);
-			if (value != null) {
+			if ( value != null ) {
 				return value;
 			}
 		}
 		return null;
 	}
 
-	public boolean getBoolean(String key, boolean defaultVal) {
+
+	public boolean getBoolean( String key, boolean defaultVal ) {
 		String val = get(key);
 		return val == null ? defaultVal : Boolean.parseBoolean(val);
 	}
 
-	public long getLong(String key, long defaultVal) {
+
+	public long getLong( String key, long defaultVal ) {
 		String val = get(key);
 		return val == null ? defaultVal : Long.parseLong(val);
 	}
 
-	public int getInt(String key, int defaultVal) {
+
+	public int getInt( String key, int defaultVal ) {
 		return (int) getLong(key, defaultVal);
 	}
 
-	public double getDouble(String key, double defaultVal) {
+
+	public double getDouble( String key, double defaultVal ) {
 		String val = get(key);
 		return val == null ? defaultVal : Double.parseDouble(val);
 	}
 
-	public <T extends Enum<T>> T getEnum(String key, Class<T> type, T defaultValue) {
+
+	public <T extends Enum<T>> T getEnum( String key, Class<T> type, T defaultValue ) {
 		String val = get(key);
 		return val == null ? defaultValue : Enum.valueOf(type, val);
 	}
