f9a22b43d5bf99e99780e3c5c5d53478d65a277a
青苗 <=>
2015-12-22 22:13:06 +0800

    为简化跨域努力中，休息了

--- kisso/src/main/java/com/baomidou/kisso/web/handler/KissoDefaultHandler.java
+++ kisso/src/main/java/com/baomidou/kisso/web/handler/KissoDefaultHandler.java
@@ -45,13 +45,13 @@ public class KissoDefaultHandler implements SSOHandlerInterceptor {
 	}
 
 	public boolean preTokenIsNullAjax(HttpServletRequest request, HttpServletResponse response) {
-		/*
-		 * 未登录时，处理 AJAX 请求。
-		 */
 		try {
+			/*
+			 * 未登录时，处理 AJAX 请求。
+			 */
 			response.setContentType("text/html;charset=" + SSOConfig.getSSOEncoding());
 			PrintWriter out = response.getWriter();
-			out.print("{flag:\"-1\",msg:\"login timeout, please login again.\"}");
+			out.print("prompt login exception, please login again.");
 			out.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -61,24 +61,6 @@ public class KissoDefaultHandler implements SSOHandlerInterceptor {
 
 	public boolean preTokenIsNull(HttpServletRequest request, HttpServletResponse response) {
 		/* 预留子类处理 */
-//		/*
-//		 * 当前角色非空，并且为业务系统, 执行跨域逻辑。
-//		 * <p>
-//		 * 1、发出请求询问 SSO 是否登录, 私钥签名 authToken 并设置临时 cookie
-//		 * </p>
-//		 */
-//		String role = SSOConfig.getInstance().getRole();
-//		if (!"".equals(role) && !SSOConfig.SSO_ROLE.equals(role)) {
-//			String myPrivateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMp4ORF5r+mBfYrpX944JDyQiaKof5CQ43H65tWS8wT2avQjiUSU2vweXVDbMRD7PPjOAPI5aiIIHFLBqAE9Nq17Xloyz66mUkvvhJxbmB24FhyhB8dREL85EIUahrljrTj2nWWGjDfXIqQoY1NhBi6l2VHgcVuoRI48UzAWcbSBAgMBAAECgYAtLeaOH7lBQcPh23GpBJ4RZa9QvIi6mZonNPWNct0HnnT/RW67/vtehugLwt2QDH/uhQlxA57LOUQYs13p6N7qMZ+4YY592hw4hrJUEAuuORU+wKWnr+wVQNm6Qc9Qf7axM6B5NgtLPbf0R7M53vgHHMyJh2tJKrY3RUdBbsUugQJBAObj3+B7v2QVKKPZlYvICwbKZAUcb1qZtPjtw7+aDah0EEqkaYD0ytmjl2esoknPySN2gbouc+nDvYZopFLgiDMCQQDgfRqCYfMHhjHPHoOwco3ZAevDDe22QksBIkfgFB9srEJCWauFyvB5PTG6+wFv94zqy3R92C6AVaWn8Ae8uqx7AkBkroWXfB7PY7KfEGh31bmJMoQ+/lFIbrJNwlCTonfGNyZLhjpDc3tpQD7rhIoYKbWJ80lKiKsfCq4AiGzvft2lAkEAqcBQDGmu0XC7N2hWolVtR7x5H8znhNuKRfg7K4lr3cxAalXOKuSzhKoucbqecqFZsK5aj1Kqjya0llIeN6tdAwJAImLxsxLxhk6dc8slEo8ObLAWWWkRZNiXCpr+2aWspVx1cK3GRtAa+0Q7X0TiA62/CrlWR/xJHvDI/+I9mcxJKg==";
-//			String axt = SSOHelper.askCiphertext(request, response, myPrivateKey);
-//			HttpUtil.sendRedirect(response, "http://sso.test.com:8080/replylogin.html?askTxt=" + axt);
-//		} else {
-//			/*
-//			 * 清理登录状态并重定向至登录界面
-//			 */
-//			logger.fine("logout. request url:" + request.getRequestURL());
-//			SSOHelper.clearRedirectLogin(request, response);
-//		}
 		return true;
 	}
 
